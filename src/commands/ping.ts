1a4df3c01b7826ad204b72b1aff9e6624c47f7a1
Delemangi <milev.stefan@gmail.com>
2026-02-01 23:29:19 +0100

    Register ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,6 +1,6 @@
 import type { CommandInteraction } from 'discord.js';
 
-import { Command } from '@sapphire/framework';
+import { type ApplicationCommandRegistry, Command } from '@sapphire/framework';
 
 export class PingCommand extends Command {
   constructor(context: Command.LoaderContext, options: Command.Options) {
@@ -13,5 +13,11 @@ export class PingCommand extends Command {
 
   override async chatInputRun(interaction: CommandInteraction) {
     await interaction.reply('Pong!');
+  }
+
+  override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+    registry.registerChatInputCommand((builder) =>
+      builder.setName(this.name).setDescription(this.description),
+    );
   }
 }
